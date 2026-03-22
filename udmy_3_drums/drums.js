0b5fa71ac1b25c27d71fa1dfc6d71130e489febb
aguttero <aguttero@gmail.com>
2026-03-22 17:16:23 -0300

    0322pm02

--- udmy_3_drums/drums.js
+++ udmy_3_drums/drums.js
@@ -1,32 +1,39 @@
 // TEST Code
 // alert("js link test ok");
 
-document.querySelector("button").addEventListener("click", handleClick);
+// document.querySelector("button").addEventListener("click", handleClick);
 
 // with anonimous function:
-document.querySelector("button").addEventListener("click", function () {
-    alert("Clickeeed!");
-});
+// document.querySelector("button").addEventListener("click", function () {
+//     alert("Clickeeed!");
+// });
 
 // para todos los botones con querySelectorAll
 // 1. Seleccionamos todos los botones
-const botones = document.querySelectorAll("button");
+// const botones = document.querySelectorAll("button");
 
-// 2. Recorremos cada botón de la lista
-botones.forEach(function(boton) {
-    // 3. Le asignamos el evento a "cada uno"
-    boton.addEventListener("click", function() {
-        alert("¡Hiciste clic en un botón!");
-    });
-});
+// // 2. Recorremos cada botón de la lista
+// botones.forEach(function(boton) {
+//     // 3. Le asignamos el evento a "cada uno"
+//     boton.addEventListener("click", function() {
+//         alert("¡Hiciste clic en un botón!");
+//     });
+// });
 
-// 3. For Tradicional
+// 3. For loop - Tradicional
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll("button")[i].addEventListener("click", function () {
-        alert("FOR Clickeeed!");
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        this.style.color = "white";
+        console.log(this.innerHTML);
+        console.log(this.innerText);
+        var audio = new Audio("sounds/tom-1.mp3");
+        audio.play();
+
+        // alert("FOR Clickeeed!");
     })
 }
-
 
 function handleClick() {
     alert("Clicked!");
