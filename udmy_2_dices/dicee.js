7fc4c1a00e8e86f2667a43bfb93dd11fa70aeee2
aguttero <aguttero@gmail.com>
2026-03-22 10:13:20 -0300

    0322am 01 roll dice js done

--- udmy_2_dices/dicee.js
+++ udmy_2_dices/dicee.js
@@ -3,7 +3,30 @@ function randomDice(){
     return randomNumber;
 }
 
-for (var counter = 1; counter < 51; counter++){
-    console.log("dice "+counter, randomDice());
+// roll dices for each player:
+dicePlayer1 = randomDice();
+dicePlayer2 = randomDice();
+
+// udpate dice image
+// by using setAttrbute
+//document.querySelector(".img1").setAttribute("src", "./images/dice4.png");
+
+// using direct property .src
+document.querySelector(".img1").src = `images/dice${dicePlayer1}.png`;
+document.querySelector(".img2").src = `images/dice${dicePlayer2}.png`;
+
+// update h1
+if (dicePlayer1 === dicePlayer2) {
+    document.querySelector("h1").innerText = "Draw";
+} else if (dicePlayer1 > dicePlayer2) {
+    document.querySelector("h1").innerText = "🚩 P1 wins!";
+} else {
+    document.querySelector("h1").innerText = "P2 wins! 🚩";
 
 }
+
+// TEST CODE
+// for (var counter = 1; counter < 51; counter++){
+//     console.log("dice "+counter, randomDice());
+
+// 
